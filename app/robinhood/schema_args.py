26637194e285d66facb6ae6c38ee031c7ede7f28
StMedrano <159828399+StMedrano@users.noreply.github.com>
2026-09-18 17:44:52 -0500

    Support account and date args in live MCP schemas

--- app/robinhood/schema_args.py
+++ app/robinhood/schema_args.py
@@ -8,6 +8,9 @@ ALIASES: dict[str, tuple[str, ...]] = {
     "option_ids": ("option_ids", "instrument_ids", "ids", "option_instrument_ids"),
     "expiration_date": ("expiration_date", "expiry", "expiration"),
     "option_type": ("option_type", "type", "side"),
+    "account_number": ("account_number", "account_id", "rhs_account_number"),
+    "start_date": ("start_date", "from_date", "start"),
+    "end_date": ("end_date", "to_date", "end"),
 }
 
 
